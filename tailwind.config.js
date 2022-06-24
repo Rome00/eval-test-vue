33fc390359b9ed277bcedd3b58aafb39baa29882
Rome00 <r.khazalia@gmail.com>
2022-06-24 15:38:51 +0400

    config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,27 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./vendor/laravel/jetstream/**/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.vue",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
-                Hind: ["Hind", "sans-serif"],
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                Hind: ['Hind', 'sans-serif'],
             },
             colors: {
-                brand: "#1B998B",
+                brand: '#1B998B',
+            },
+            boxShadow: {
+                full: `0px 8px 40px rgba(1, 33, 54, 0.05)`,
             },
         },
     },
 
-    plugins: [
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/typography"),
-    ],
-};
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
